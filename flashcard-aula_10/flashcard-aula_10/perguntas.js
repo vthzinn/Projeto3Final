dbba641366c90cc32151aa6f227abc0cf556f2c3
vthzinn <00001109493654SP@al.educacao.sp.gov.br>
2024-11-13 08:43:10 -0300

    Add files via upload

---
+++ flashcard-aula_10/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'LITERATURA',
+    'Quem Escreveu, o livro "O Princípe"?',
+    ' Nicolau Maquíavel'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Brasil?',
+    'Brasília'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 1+1?',
+    'O resultado é 2'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Você em Inglês?',
+    'Você em ingles é You '
+)
